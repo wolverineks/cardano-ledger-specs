41e0c0b86d9edb6b784420daa9564db8a76542aa
Jared Corduan <jared.corduan@iohk.io>
2021-05-17 13:57:07 -0400

    Introduces era parmetric Fields, and updaters
    The parmetric fields can be used with the updaters to apply a field to
    an appropriate objects from any era.  Also introduced is the Policy type
    that controls how the value in the field is used to update the object.
    
    Using function calls like: newXXX policy eraWitness [field1,..,fieldn]
    one can create new objects where the missing fields are given default
    values.

---
+++ cardano-ledger-test/test/Tests.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import Test.Cardano.Ledger.Examples.TwoPhaseValidation
+  ( alonzoBBODYexamples,
+    alonzoUTXOWexamples,
+  )
+import Test.Tasty
+import Test.TestScenario (TestScenario (..), mainWithTestScenario)
+
+-- ====================================================================================
+
+tests :: TestTree
+tests = askOption $ \case
+  Nightly -> mainTests
+  Fast -> mainTests
+  _ -> mainTests
+
+mainTests :: TestTree
+mainTests =
+  testGroup
+    "STS Tests"
+    [ alonzoUTXOWexamples,
+      alonzoBBODYexamples
+    ]
+
+-- main entry point
+main :: IO ()
+main = mainWithTestScenario tests
