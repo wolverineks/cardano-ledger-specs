7f6508752baafe66263e8906478fbeb851b7517d
Jared Corduan <jared.corduan@iohk.io>
2021-09-17 18:09:13 -0400

    deprecate CHAIN and TICKN modules

---
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Chain.hs
@@ -0,0 +1,6 @@
+module Shelley.Spec.Ledger.STS.Chain
+  {-# DEPRECATED "Use 'import Cardano.Ledger.Chain' instead." #-}
+  (module X)
+where
+
+import Cardano.Ledger.Chain as X
---
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Tickn.hs
@@ -0,0 +1,6 @@
+module Shelley.Spec.Ledger.STS.Tickn
+  {-# DEPRECATED "Use 'import Cardano.Protocol.TPraos.Rules.Tickn' instead." #-}
+  (module X)
+where
+
+import Cardano.Protocol.TPraos.Rules.Tickn as X
